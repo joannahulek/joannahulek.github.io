82387fdee825e6350c5863d791cb0a1b08a5e5f8
Joanna Hulek <joanna.hulek@gmail.com>
2024-04-20 19:24:47 +0200

    footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+        <footer className="border-top">
+        <p className="text-center text-body-secondary">© 2024 Joanna Hulek</p>
+    </footer>
+    )
+}
+
+export default Footer
