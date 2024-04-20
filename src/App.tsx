dd12c686e61c10368a50fe1681c2d142b4636db7
Joanna Hulek <joanna.hulek@gmail.com>
2024-04-20 20:46:59 +0200

    lorem ipsum added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AboutMe from "./components/Bio";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/CV";
 import Contact from "./components/Contact";
+import bio from "./components/content/Bio.json"
 import tableContent from "./components/content/CV.json"
 
 
@@ -13,7 +14,7 @@ function App() {
   return (
       <div className="content">
           {Navbar()}
-          <section id="#bio">{AboutMe()}</section>
+          <section id="#bio"><AboutMe bio={bio}/></section>
           <section id="#portfolio">{Portfolio()}</section>
           <section id="#cv"><Resume items={tableContent.Col}/></section>
           <section id="#contact">{Contact()}</section>
--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,5 +1,10 @@
 import image from "./images/JH_lifestyle_foto.jpg"
-function AboutMe(){
+
+interface Props {
+    bio: string[]
+}
+
+function AboutMe(props: Props){
     return(
         <div className="card">
             <div className="row g-0">
@@ -8,13 +13,9 @@ function AboutMe(){
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with
-                            supporting text below as a natural lead-in to additional content. This content is a little
-                            bit longer.</p>
-                        <a href="https://getbootstrap.com/docs/5.3/components/card/#horizontal" target="_blank"><h6>LINK</h6></a>
-                        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small>
-                        </p>
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text"><big>{props.bio}</big></p>
+                        <p className="card-text">{props.bio}</p>
                     </div>
                 </div>
             </div>
