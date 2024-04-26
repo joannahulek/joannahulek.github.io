4a496119392095b90c5343cabf241975faa8690f
Joanna Hulek <joanna.hulek@gmail.com>
2024-04-26 12:04:10 +0200

    hero links added

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,19 @@ import AboutMe from "./components/Bio";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/CV";
 import Contact from "./components/Contact";
-import bio from "./components/content/Bio.json"
-import tableContent from "./components/content/CV.json"
 import Hero from "./components/Hero";
+import bio from "./components/content/Bio.json"
+import resume from "./components/content/CV.json"
+import links from "./components/content/links.json"
 
 function App() {
   return (
       <div className="content">
           {Navbar()}
-          <Hero />
+          <Hero links={links.Main}/>
           <section id="bio"><AboutMe bio={bio}/></section>
           <section id="portfolio">{Portfolio()}</section>
-          <section id="cv"><Resume items={tableContent.Col}/></section>
+          <section id="cv"><Resume items={resume.Col}/></section>
           <section id="contact">{Contact()}</section>
           {Footer()}
       </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,10 @@
 import image from "./images/JH_lifestyle_foto.jpg"
 
-function Hero(){
+interface Props {
+    links: string[][];
+}
+
+function Hero(props: Props){
     return(
         <div className="container col-xxl-8 px-4 py-5">
             <div className="row align-items-center g-5 py-5">
@@ -13,8 +17,11 @@ function Hero(){
                         world’s most popular front-end open source toolkit, featuring Sass variables and mixins,
                         responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
                     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                        <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+                        {props.links.map((link) => (
+                            <a href={link[0]} type="button"
+                               className="btn btn-outline-secondary btn-lg px-4">{link[1]}</a>
+
+                        ))}
                     </div>
                 </div>
                 <div className="col-10 col-sm-8 col-lg-4">
@@ -28,12 +35,3 @@ function Hero(){
 }
 
 export default Hero
-
-
-/*
-
-                    <h2 className="text-center">Frontend Developer</h2>
-                    <h2 className="text-center">& Agile Practitioner</h2>
-
-
-* */
