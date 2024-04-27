98047edce015e155cbd798282ef54962f79990d5
Joanna Hulek <joanna.hulek@gmail.com>
2024-04-27 11:34:24 +0200

    CV rendering created

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -1,49 +1,45 @@
 interface Props {
-    items: string[][];
-    work: string[][][]
+    work: string[][][];
+    education: string[][][];
 }
 
 function Resume (props: Props) {
     return (
         <>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">color</th>
-                    <th scope="col">value</th>
-                </tr>
-                </thead>
-                <tbody>
-                {props.items.map((item, index) => (
-                        <tr>
-                            <th scope="row">{index + 1}</th>
-                            <td>{item[0].toString()}</td>
-                            <td>{item[1].toString()}</td>
-                        </tr>
-                    )
-                )
-                }
-                </tbody>
-            </table>
-
-            <table className="table">
-
-                {props.work.map((item) => (
-                        <tr>
-                            <td>{item[0][0]}</td>
-                            <td>{item[0][1]}</td>
-                            <td>{item[0][2]}</td>
-                            <td>{item[1]}</td>
-                        </tr>
-                    )
-                )
-                }
-            </table>
-
-
+            {renderTable("Work Experience", props.work)}
+            {renderTable("Education", props.education)}
         </>
     )
 }
 
 export default Resume
+
+function renderTable(title:string, items:string[][][]) {
+    return <table className="table">
+        <tr>
+            <thead>
+                <th>{title}</th>
+            </thead>
+            <td>
+                {items.map((item) => (
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th colSpan={2}>{item[0][0]}</th>
+                            </tr>
+                            <tr>
+                                <td>{item[0][1]}</td>
+                                <td>{item[0][2]}</td>
+                            </tr>
+                            </thead>
+                            <tr>
+                                <td colSpan={2}>{item[1][0]}</td>
+                            </tr>
+                        </table>
+                    )
+                )
+                }
+            </td>
+        </tr>
+    </table>;
+}
