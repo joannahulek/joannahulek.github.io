8207c6d3537a94f905c84b8e3b5e0f81a715d874
JH <joanna.hulek@gmail.com>
2024-07-06 19:19:11 +0200

    portfolio components placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import AboutMe from "./components/Bio";
 import Portfolio from "./components/Portfolio";
+import Projects from "./components/content/Projects";
+import PublicSpeaking from "./components/content/PublicSpeaking";
+import Initiatives from "./components/content/Initiatives";
 import CV from './components/CV';
 import Contact from "./components/Contact";
 import content from "./components/content/content.json"
@@ -19,13 +22,16 @@ function App() {
               links={links.main}/>
           <section id="bio"><AboutMe bio={content.bio} links={links.main}/></section>
           <section id="portfolio">{Portfolio()}</section>
+          <section id="projects">{Projects()}</section>
+          <section id="public_speaking">{PublicSpeaking()}</section>
+          <section id="initiatives">{Initiatives()}</section>
           <section id="cv">
               <h1>My Resume</h1>
-            {CV()}
-                 {/* ** Resume with content imported from JSON
+              {CV()}
+              {/* ** Resume with content imported from JSON
             <Resume work={resume.work} education={resume.education} />
                   */}
-        </section>
+          </section>
           <section id="contact"><Contact/></section>
           {Footer()}
       </div>
