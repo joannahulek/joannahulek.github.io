6fbffb7d846409dcf74bc2e0566064f6133d81c8
JH <joanna.hulek@gmail.com>
2024-07-12 14:46:05 +0200

    text adjusted

--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -5,7 +5,7 @@ const projects: PortfolioItem[]=[
         title: "Joanna Hulek - My home website",
         time: "from April 2024",
         description: <><p>This is the website you are currently visiting:</p>
-            <a href="https://github.com/joannahulek/joannahulek.github.io" className="card-link">GitHub Repository</a></>,
+            <a href="https://github.com/joannahulek/joannahulek.github.io" className="card-link">Project repository</a></>,
         link: <><p className="card-text">If you have any questions, suggestions and ideas, or get know about my other
             projects - feel free to contact me!</p>
             <a href="mailto:joanna.hulek@gmail.com?&subject=E-mail%20from%20website&body=Hi%20Joanna,%20"
@@ -18,23 +18,22 @@ const projects: PortfolioItem[]=[
         time: "May - Jun 2024",
         description: <>Together with Marta, we are collaborating to create a minimalist business card that will help her acquire clients and present her portfolio and offer.</>,
         link: <><a href="https://marka-tekst.vercel.app/" target="_blank"
-                   className="card-link">MarKa Tekst - business card website</a>
+                   className="card-link">MarKa Tekst - website</a>
             <br></br>
             <a href="https://github.com/joannahulek/marka"
-               target="_blank" className="card-link">GitHub repository</a></>
+               target="_blank" className="card-link">Project repository</a></>
     },
     {
         title: "Other Projects",
         time: "",
         description: <><p>
-            I'm looking for opportunities with React, TypeScript, and JavaScript. I've experience in creating
-            responsive websites and mobile apps, focusing on readable code and best practices. I'm eager to learn
-            automated testing to deliver best solutions.
+            I'm working with React, TypeScript, and JavaScript and I've experience in creating responsive websites, focusing on readable code and best practices.
         </p>
-            <p>I'd be happy to discuss what I can bring to your company as an employee and developer.</p>
+            <p>
+                I'd be happy to discuss how can I help you in your project, business or what I can bring to your company as an employee and developer.
+            </p>
         </>,
-        link: <a href="https://github.com/joannahulek/" target="_blank" className="card-link">Joanna Hulek on
-            GitHub</a>
+        link: <a href="https://github.com/joannahulek/" target="_blank" className="card-link">My projects on GitHub</a>
     }
 ]
 
