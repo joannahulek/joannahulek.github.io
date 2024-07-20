00929544f7eb63880dc65c9d3d2ceb4f8bcf761f
Joanna Hulek <joanna.hulek@gmail.com>
2024-07-20 12:41:30 +0200

    MarKa images added

--- src/components/content/Projects.tsx
+++ src/components/content/Projects.tsx
@@ -1,4 +1,9 @@
 import Portfolio, {PortfolioItem} from "../Portfolio";
+import MarKa1 from "../images/portfolio/MarKa1.jpg"
+import MarKa2 from "../images/portfolio/MarKa2.jpg"
+import MarKa3 from "../images/portfolio/MarKa3.jpg"
+
+let MarkaImages = [MarKa1, MarKa2, MarKa3]
 
 const projects: PortfolioItem[]=[
     {
@@ -16,12 +21,23 @@ const projects: PortfolioItem[]=[
     {
         title: "MarKa Tekst - business website",
         time: "May - Jun 2024",
-        description: <>Together with Marta, we are collaborating to create a minimalist business card that will help her acquire clients and present her portfolio and offer.</>,
+        description:
+            <div className="row">
+                <div className="col">
+                    <a href="https://marka-tekst.vercel.app/" target="_blank"
+                       className="card-link">
+                        <img src={MarKa1} alt="MarKa Tekst - website" title="MarKa Tekst - website" width="100%" loading="lazy"/>
+                    </a>
+                </div>
+                <div className="col">
+                    Together with Marta, we are collaborating to create a minimalist business card that will help her
+                    acquire clients and present her portfolio and offer.
+                </div>
+            </div>,
         link: <><a href="https://marka-tekst.vercel.app/" target="_blank"
-                   className="card-link">MarKa Tekst - website</a>
-            <br></br>
+                   className="card-link">Website</a>
             <a href="https://github.com/joannahulek/marka"
-               target="_blank" className="card-link">Project repository</a></>
+               target="_blank" className="card-link">Repository</a></>
     },
     {
         title: "Other Projects",
